07e7bd1337c36147ac579d429c430157c913c15f
aissy <aissypewdiepie@gmail.com>
2020-12-28 00:47:30 +0530

    understood a little of type dynamics

--- test.py
+++ test.py
@@ -6,32 +6,32 @@ from tensor_annotations.tensorflow import Tensor1, Tensor0
 def globalize(f):
     global dic, tensor0
     dic = {"x":int, "y":float}
-    def new(**dic1):
+    def new(type_, **dic1):
         x,y = dic1
         tensor0 =Tensor0[dic1[x]]
-        return tensor0
+        return f(tensor0, dic)
     return new    
 
 class d_tensor(Tensor0):
     # this needs to be difined
     # x,y = dic
-    def __init__(self):
-        self.A1 = Tensor1[self.test()]
+    def __init__(self, type_):
+        self.A1 = Tensor1[type_]
 
 class d_tensor1(object):
     def setdic(data1, data2):
         for x,y in zip(data1, data2):
             dic1[x] = int
             dic1[y] = float
-
+    
+    # TODO expand the tensor classes 
     @globalize
-    def main(dic):
-        t = d_tensor()
-        return dic
+    def main(type_,dic):
+        d_t = d_tensor(type_)
+        return d_t.A1
 
     def __init__(self, **dic):
-        dic | dic1
-        self.t0 = d_tensor1.main(**dic)
+        self.t0 = d_tensor1.main(int, **dic)
         
 
 def run():
